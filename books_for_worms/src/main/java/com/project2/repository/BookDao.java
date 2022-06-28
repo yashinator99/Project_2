1eb73785af7b226f46cd67d5347be1fec8523da4
Jeremy Gray <jgray@brainstation.io>
2022-06-28 19:49:01 -0400

    added book insert for BookDao

--- books_for_worms/src/main/java/com/project2/repository/BookDao.java
+++ books_for_worms/src/main/java/com/project2/repository/BookDao.java
@@ -14,20 +14,34 @@ public class BookDao implements BookDaoInterface {
 
     @Override
     public void insert(BookEntity bookEntity) {
-        // TODO Auto-generated method stub
         Connection connection = ConnectionFactory.getConnection();
         String sql = "INSERT INTO books (?, ?, ?, ?, ?, ?, ?) ;";
 
         try {
-            PreparedStatement preparedStatement = connection.
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, bookEntity.getBook_id());
+            preparedStatement.setString(2, bookEntity.getTitle());
+            preparedStatement.setString(3, bookEntity.getAuthor());
+            preparedStatement.setString(4, bookEntity.getGenre());
+            preparedStatement.setDate(5, bookEntity.getYear());
+            preparedStatement.setBoolean(6, bookEntity.isFiction());
+            preparedStatement.setString(7, bookEntity.getDescription());
 
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                return;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return;
 
     }
 
     @Override
     public BookEntity select(int book_id) {
-        // TODO Auto-generated method stub
         Connection connection = ConnectionFactory.getConnection();
         String sql = "SELECT * FROM books WHERE book_id=? ;";
 
