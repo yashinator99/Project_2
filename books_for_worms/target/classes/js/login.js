31dc3dd56f3f079e183b69bda6feb22f58ce2056
Yash <yashinator99@gmail.com>
2022-06-29 19:20:31 -0400

    updates login js

--- books_for_worms/target/classes/js/login.js
+++ books_for_worms/target/classes/js/login.js
@@ -1,14 +1,15 @@
 const form = document.getElementById("login");
-const username = form.elements['username'];
-const password = form.elements['password'];
-let usernameVal = username.value;
-let passwordVal = password.value;
+const username = form.elements["username"];
+const password = form.elements["password"];
+
 
 
 form.addEventListener('submit', (event) => {
     // handle the form data
     event.preventDefault();
-    console.log("username: " + username);
-    console.log("password: " + password);
+    let usernameVal = username.value;
+    let passwordVal = password.value;
+    console.log("username: " + usernameVal);
+    console.log("password: " + passwordVal);
 
 });
