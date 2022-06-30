02d3cdc76fc2423edf1329a9626db3548b5a5326
Yash <yashinator99@gmail.com>
2022-06-30 14:50:14 -0400

    add test structure

--- books_for_worms/src/main/java/com/project2/App.java
+++ books_for_worms/src/main/java/com/project2/App.java
@@ -12,11 +12,8 @@ import com.project2.repository.entities.BookEntity;
 import com.project2.repository.entities.LibraryEntity;
 import com.project2.repository.entities.UserEntity;
 import com.project2.util.ConnectionFactory;
-<<<<<<< HEAD
 
 import static io.javalin.apibuilder.ApiBuilder.*;
-=======
->>>>>>> c96628c8172e1d11593322ccb5a9f4ca9aaa5e77
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
@@ -33,52 +30,11 @@ public class App {
                 });
         app.start(9090);
 
-<<<<<<< HEAD
         app.routes( () -> {
             path("register", () -> {
                 app.get("/", UserController.registerHandler);
             });
-            
-=======
-        app.routes(() -> {
-            app.get("/", ctx -> {
-                ctx.redirect("/homepage.html");
-            });
 
-            BookDao newBookDao = new BookDao();
-
-            app.post("/homepage.html", ctx -> {
-                String searchCategory = ctx.formParam("select");
-                String searchTerm = ctx.formParam("search");
-                
-                if (newBookDao.search(searchCategory, searchTerm) != null){
-                    BookEntity foundBook = newBookDao.search(searchCategory, searchTerm);
-                    ctx.json(foundBook);
-                } else{
-                    ctx.json("No book found with that criteria");
-                }
-            });
-
-            app.get("/recommendation.html", ctx -> {
-                BookEntity randomBook = newBookDao.random();
-                ctx.json(randomBook);
-            });
-
-            /*
-            app.get("/book/{name}", ctx -> {
-                try{
-                    String book = ctx.pathParam("name");
-                    int bookID = Integer.parseInt(book);
-                    BookEntity foundBook = newBookDao.search(bookID);
-                    ctx.json(foundBook);
-
-                } catch (Exception e){
-                    e.printStackTrace();
-                    ctx.result("No book found with that criteria");
-                };
-            });
-             */
->>>>>>> c96628c8172e1d11593322ccb5a9f4ca9aaa5e77
         });
 
         // app.routes(() -> {
