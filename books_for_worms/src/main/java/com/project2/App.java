30ab67761ca77ec3c8c1aa32c4f2f00d2904ffec
Emmanuel Oti <otiemmanuel3710@gmail.com>
2022-06-30 12:34:54 -0400

    I added folders for service, models, and controller

--- books_for_worms/src/main/java/com/project2/App.java
+++ books_for_worms/src/main/java/com/project2/App.java
@@ -4,6 +4,7 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.project2.controller.UserController;
 import com.project2.repository.BookDao;
 import com.project2.repository.LibraryDao;
 import com.project2.repository.UserDao;
@@ -12,6 +13,7 @@ import com.project2.repository.entities.LibraryEntity;
 import com.project2.repository.entities.UserEntity;
 import com.project2.util.ConnectionFactory;
 
+import static io.javalin.apibuilder.ApiBuilder.*;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 
@@ -28,39 +30,52 @@ public class App {
                 });
         app.start(9090);
 
-        app.routes(() -> {
-            app.get("/", ctx -> {
-                ctx.redirect("/homepage.html");
+        app.routes( () -> {
+            path("register", () -> {
+                app.get("/", UserController.registerHandler);
             });
-
-            BookDao newBookDao = new BookDao();
-
-            app.post("/homepage.html", ctx -> {
-                String searchCategory = ctx.formParam("select");
-                String searchTerm = ctx.formParam("search");
-                int bookID = 0;
-                if (newBookDao.search(searchCategory, searchTerm) != null){
-                    bookID = newBookDao.search(searchCategory, searchTerm).getBook_id();
-                }
-                ctx.redirect("/book/" + bookID);
-            });
-
-            app.get("/recommendation.html", ctx -> {
-                int bookID = newBookDao.random().getBook_id();
-                ctx.redirect("/book/" + bookID);
-            });
-
-            app.get("/book/{name}", ctx -> {
-                try{
-                    int bookID = Integer.parseInt((ctx.pathParam("name")));
-                    BookEntity foundBook = newBookDao.search(bookID);
-                    ctx.json(foundBook);
-
-                } catch (Exception e){
-                    ctx.result("No book found with that criteria");
-                };
-            });
+            
         });
+
+        // app.routes(() -> {
+        //     path("register", () -> {
+        //         app.get("/", UserController.registerHandler);
+        //     });
+        // });
+
+        // app.routes(() -> {
+        //     app.get("/", ctx -> {
+        //         ctx.redirect("/homepage.html");
+        //     });
+
+        //     BookDao newBookDao = new BookDao();
+
+        //     app.post("/homepage.html", ctx -> {
+        //         String searchCategory = ctx.formParam("select");
+        //         String searchTerm = ctx.formParam("search");
+        //         int bookID = 0;
+        //         if (newBookDao.search(searchCategory, searchTerm) != null){
+        //             bookID = newBookDao.search(searchCategory, searchTerm).getBook_id();
+        //         }
+        //         ctx.redirect("/book/" + bookID);
+        //     });
+
+        //     app.get("/recommendation.html", ctx -> {
+        //         int bookID = newBookDao.random().getBook_id();
+        //         ctx.redirect("/book/" + bookID);
+        //     });
+
+        //     app.get("/book/{name}", ctx -> {
+        //         try{
+        //             int bookID = Integer.parseInt((ctx.pathParam("name")));
+        //             BookEntity foundBook = newBookDao.search(bookID);
+        //             ctx.json(foundBook);
+
+        //         } catch (Exception e){
+        //             ctx.result("No book found with that criteria");
+        //         };
+        //     });
+        // });
 
     }
 }
